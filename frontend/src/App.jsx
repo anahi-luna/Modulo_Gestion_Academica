b36f48d6cf61be88f9c6e8183f67284388a1a228
Blancat2 <agustinblancat@gmail.com>
2026-06-27 19:02:15 +0000

    agregado de cambios entre admin y user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,41 @@
 
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "../components/navbar"
-import Inscripciones from "../pages/Inscripciones"
+import Navbar from "./components/navbar"
+import Inscripciones from "./pages/Inscripciones"
+import HomeAdmin from "./pages/HomeAdmin"
+import { adminMock, userMock } from './Services/mockUsers';
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 
 
 export default function App() {
 
-
+  const [usuario, setUsuario] = useState(userMock)
     
   return (
 
     <BrowserRouter>
 
       <>
-        <Navbar/>
+        <Navbar
+          usuario = {usuario}
+          setUsuario = {setUsuario}
+        />
         
         
         <main>
 
           <Routes>
-              <Route path="/"     element={<Inscripciones/>}></Route>
+              <Route
+                path="/"
+                element={
+                  usuario.rol === "ADMIN"
+                  ? <HomeAdmin />
+                  : <Inscripciones />
+                }
+              />
           </Routes>
         </main>
       </>
---
+++ frontend/src/Services/mockUsers.js
@@ -0,0 +1,11 @@
+export const adminMock = {
+  id: 1,
+  nombre: "Administrador",
+  rol: "ADMIN",
+};
+
+export const userMock = {
+  id: 2,
+  nombre: "Juan Pablo Gonzalez",
+  rol: "USER",
+};
