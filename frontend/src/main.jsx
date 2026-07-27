b6feb299629ebf05b7c2cc95a2ea0d085eb8689e
anahi-luna <rebeluna263@gmail.com>
2026-07-27 16:34:25 -0300

    se agrega basename a App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { AuthProvider } from './auth/context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/inscripciones">
       <AuthProvider>
         <App /> 
       </AuthProvider>
