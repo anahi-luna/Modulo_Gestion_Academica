f8f6897c4a0a9e4915ec56a153f7bd64b12003c6
anahi-luna <rebeluna263@gmail.com>
2026-07-29 14:05:56 -0300

    se eliminan mocks y archivos sin usar

--- backend/services/legajo_cliente.py
+++
@@ -1,7 +0,0 @@
-from mocks.mock_legajos import LEGAJOS
-
-def obtener_legajo(id_legajo):
-    for legajo in LEGAJOS:
-        if legajo["id_legajo"]== id_legajo:
-            return legajo
-    return None
