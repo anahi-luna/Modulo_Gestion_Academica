bc0ea7cc419a1fe06a18bab5ec4770c196aaed0a
anahi-luna <rebeluna263@gmail.com>
2026-08-07 23:30:13 -0300

    agregado

--- backend/routes/catalogo_routes.py
+++ backend/routes/catalogo_routes.py
@@ -7,30 +7,30 @@ catalogo_bp = Blueprint("catalogo_bp", __name__)
 
 # Estados
 catalogo_bp.route("/estados-inscripcion", methods=["GET"])(
-    requires_permission("estados_inscripcion.leer")(get_estados_inscripcion)
+    requires_permission("inscripcion.estados_inscripcion.leer")(get_estados_inscripcion)
 )
 
 catalogo_bp.route("/estados-asistencia", methods=["GET"])(
-    requires_permission("estados_asistencia.leer")(get_estados_asistencia)
+    requires_permission("inscripcion.estados_asistencia.leer")(get_estados_asistencia)
 )
 
 catalogo_bp.route("/estados-academicos", methods=["GET"])(
-    requires_permission("estados_academicos.leer")(get_estados_academicos)
+    requires_permission("inscripcion.estados_academicos.leer")(get_estados_academicos)
 )
 
 catalogo_bp.route("/estados-resultados-plan", methods=["GET"])(
-    requires_permission("estados_resultado_plan.leer")(get_estados_resultado_plan)
+    requires_permission("inscripcion.estados_resultado_plan.leer")(get_estados_resultado_plan)
 )
 
 catalogo_bp.route("/estados-certificados", methods=["GET"])(
-    requires_permission("estados_certificado.leer")(get_estados_certificado)
+    requires_permission("inscripcion.estados_certificado.leer")(get_estados_certificado)
 )
 
 # Tipos
 catalogo_bp.route("/tipos-evaluacion", methods=["GET"])(
-    requires_permission("tipos_evaluacion.leer")(get_tipos_evaluacion)
+    requires_permission("inscripcion.tipos_evaluacion.leer")(get_tipos_evaluacion)
 )
 
 catalogo_bp.route("/tipos-certificado", methods=["GET"])(
-    requires_permission("tipos_certificado.leer")(get_tipos_certificado)
+    requires_permission("inscripcion.tipos_certificado.leer")(get_tipos_certificado)
 )
